7dca37c4c3ae8f75e7e4b3f0569ad0bd025b5834
Igor Kaczorek <143325604+MadridistaPL@users.noreply.github.com>
2023-11-30 11:43:34 +0100

    Update

--- script/script.js
+++ script/script.js
@@ -1,6 +1,13 @@
-$("input[type=checkbox]").change(function (e) {
-  if ($("input[type=checkbox]:checked").length > 3) {
-    $(this).prop("checked", false);
-    alert("allowed only 3");
+function chkcontrol(j) {
+  var total = 0;
+  for (var i = 0; i < document.form.skills.length; i++) {
+    if (document.form.skills[i].checked) {
+      total = total + 1;
+    }
+    if (total > 3) {
+      alert("Please Select only three");
+      document.form.skills[j].checked = false;
+      return false;
+    }
   }
-});
+}
